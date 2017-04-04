fd330e68c091a8bd9a0e56705c1fc6a51f8eb325
Colby Scotta <cjscotta@us.ibm.com>
2017-04-04 09:37:35 -0400

    changed scope of this repo to be the script only.

--- README.md
+++ README.md
@@ -1,14 +1,42 @@
-# beloved-npm
+# npmjs
 
-This is currently a webscaper that renders the most popular NPM packages with download counts.
+This is currently a script that scrapes https://www.npmjs.com/ for the top 100 popular packages and gets their daily, weekly, and monthly download counts.
 
-More to come !
-
-## Installation
+## Running
 
 Clone repo
 ```bash
-$ npm install
-$ node app.js
+$ npm i
+$ node script.js
 ```
-Open a browser: localhost:3000
+
+example output:
+```
+[
+  {
+    "name": "express",
+    "daily": 461592,
+    "weekly": 2656439,
+    "monthly": 11384218
+  },
+  {
+    "name": "gulp",
+    "daily": 133349,
+    "weekly": 748169,
+    "monthly": 2980326
+  },
+  {
+    "name": "request",
+    "daily": 984247,
+    "weekly": 5722383,
+    "monthly": 24818621
+  },
+  {
+    "name": "async",
+    "daily": 1850609,
+    "weekly": 10367684,
+    "monthly": 43451451
+  }, 
+  ......
+]
+```
---
+++ script.js
@@ -0,0 +1,34 @@
+const cheerio = require('cheerio');
+const Promise = require('bluebird');
+const request = Promise.promisifyAll(Promise.promisify(require('request')));
+const _ = require('lodash');
+const jsonfile = require('jsonfile');
+
+const getPackages = () =>
+  Promise.join(
+    request('https://www.npmjs.com/browse/star?offset=0'),
+    request('https://www.npmjs.com/browse/star?offset=36'),
+    request('https://www.npmjs.com/browse/star?offset=72'),
+    (chunk1, chunk2, chunk3) => {
+      const html = chunk1.body + chunk2.body + chunk3.body;
+      const $ = cheerio.load(html);
+      const packages = $('.name').map((i, element) =>
+        $(element).text()).get();
+      return packages;
+    })
+    .then(packages =>
+      Promise.join(
+        request({ url: `https://api.npmjs.org/downloads/point/last-day/${packages.join()}`, json: true }),
+        request({ url: `https://api.npmjs.org/downloads/point/last-week/${packages.join()}`, json: true }),
+        request({ url: `https://api.npmjs.org/downloads/point/last-month/${packages.join()}`, json: true }),
+        (day, week, month) =>
+          _.map(packages, name =>
+           ({ name,
+             daily: day.body[name].downloads,
+             weekly: week.body[name].downloads,
+             monthly: month.body[name].downloads,
+           }))));
+
+getPackages()
+.then(results => jsonfile.writeFile('out.json', results, { spaces: 2 }, err => console.error(err)))
+.catch(err => console.error(err));
